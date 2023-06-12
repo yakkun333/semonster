211db2c2c55b8314eb263a566aa264fa31491ec5
Tanaka <m1m23a17@oit.ac.jp>
2023-06-12 15:39:51 +0900

    main monster

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
+    System.out.println("間違い");
     Monster monster = new Monster();
     System.out.println(monster.toString());
   }
