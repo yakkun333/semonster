a6043b879329aa5a5466b30ad1274056e92ec509
Tanaka <m1m23a17@oit.ac.jp>
2023-06-13 09:19:41 +0900

    main/player

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    Player player= new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
